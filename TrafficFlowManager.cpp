a08a099edb87eae8b49119c3aba06700641f858b
Torres Nguyen <tnguyen@inmusicbrands.com>
2022-07-12 10:19:09 +1200

    Fix input bugs

--- TrafficFlowManager.cpp
+++ TrafficFlowManager.cpp
@@ -73,40 +73,36 @@ int main()
 void gatherIntersectionInfo(IntersectionInfo& inputInfo)
 {
 	std::cout << "ROAD 1 (NORTH) CPM: ";
-	std::cin >> inputInfo.northCPMs;
-	while(!std::cin)
+	while(!(std::cin >> inputInfo.northCPMs) || inputInfo.northCPMs < 0)
 	{
-		std::cout << "NUMBER ACCEPTED ONLY. TRY AGAIN!!!" << std::endl;
+		std::cout << "INVALID INPUT. TRY AGAIN!!!" << std::endl;
 		std::cin.clear();
 		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 		continue;
 	}
 
 	std::cout << "ROAD 2 (EAST) CPM: ";
-	std::cin >> inputInfo.eastCPMs;
-	while(!std::cin)
+	while(!(std::cin >> inputInfo.eastCPMs) || inputInfo.eastCPMs < 0)
 	{
-		std::cout << "NUMBER ACCEPTED ONLY. TRY AGAIN!!!" << std::endl;
+		std::cout << "INVALID INPUT. TRY AGAIN!!!" << std::endl;
 		std::cin.clear();
 		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 		continue;
 	}
 
 	std::cout << "ROAD 3 (SOUTH) CPM: ";
-	std::cin >> inputInfo.southCPMs;
-	while(!std::cin)
+	while(!(std::cin >> inputInfo.southCPMs) || inputInfo.southCPMs < 0)
 	{
-		std::cout << "NUMBER ACCEPTED ONLY. TRY AGAIN!!!" << std::endl;
+		std::cout << "INVALID INPUT. TRY AGAIN!!!" << std::endl;
 		std::cin.clear();
 		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 		continue;
 	}
 
 	std::cout << "ROAD 4 (WEST) CPM: ";
-	std::cin >> inputInfo.westCPMs;
-	while(!std::cin)
+	while(!(std::cin >> inputInfo.westCPMs) || inputInfo.westCPMs < 0)
 	{
-		std::cout << "NUMBER ACCEPTED ONLY. TRY AGAIN!!!" << std::endl;
+		std::cout << "INVALID INPUT. TRY AGAIN!!!" << std::endl;
 		std::cin.clear();
 		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 		continue;
